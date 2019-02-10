7677e910b2b052122f1a6048bc5c867867f0f0dc
Reddy <lewismd3283@gmail.com>
2019-02-09 22:18:40 -0600

    Added some more notes to follow along with and flush out tomorrow.

--- main.py
+++ main.py
@@ -7,19 +7,32 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return # add code here "form" if not using templates
+    return # add code here "form" or string format time_form.format(hours='', hours_error='') if not using templates
 
 @app.route("/verified", methods=['POST'])
 def verified():
     # add code here
-    # username = request.form['username']
-    # password = request.form['pw']
-    # verification = request.form['pw_ver']
-    # email = request.form['email']
+    user = request.form['user']
+    password = request.form['pw']
+    verification = request.form['pw_ver']
+    email = request.form['email']
+    # validate if the text entered into the form is in the correct format
+
+def valid_username(user):
+    # add code here to verify if the user name is in the correct format. 
+
+#  add other define functions to validate the rest of the fields
+
+    user_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
     return # add return code here
 
 @app.route("/failed")
 def failed():
     # add code here
+    # return figure out the template way to return error messages
 
 app.run()
