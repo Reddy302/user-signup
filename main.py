fa29479a73f04f397dec0a1187e52b2cda3d2174
Reddy <lewismd3283@gmail.com>
2019-02-11 01:25:25 -0600

    Finished rendering templates and tested functionality. Fixed layout.

--- main.py
+++ main.py
@@ -1,38 +1,85 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# HERE IS WHERE THE HTML TEMPLATE INFORMATION GOES
 
 @app.route("/")
 def index():
-    return # add code here "form" or string format time_form.format(hours='', hours_error='') if not using templates
+    template = jinja_env.get_template('form.html')
+    return template.render()
 
-@app.route("/verified", methods=['POST'])
+
+@app.route("/", methods=['POST'])
 def verified():
-    # add code here
     user = request.form['user']
     password = request.form['pw']
     verification = request.form['pw_ver']
     email = request.form['email']
-    # validate if the text entered into the form is in the correct format
-
-def valid_username(user):
-    # add code here to verify if the user name is in the correct format. 
-
-#  add other define functions to validate the rest of the fields
 
     user_error = ''
     password_error = ''
     verify_error = ''
     email_error = ''
 
-    return # add return code here
+    if len(user) < 3 or len(user) > 20:
+        user_error = 'Usernames must be between 3 and 20 characters.'
 
-@app.route("/failed")
-def failed():
-    # add code here
-    # return figure out the template way to return error messages
+    for char in user:
+        if char == ' ':
+            user_error = 'Usernames cannot include spaces.'
+    
+    if len(password) < 3 or len(password) > 20:
+        password_error = 'Passwords must be between 3 and 20 characters.'
+        password = ''
+        verification = ''
+    
+    for char in password:
+        if char == ' ':
+            password_error = 'The password field cannot include spaces.'
+            password = ''
+            verification = ''
+
+    if password != verification:
+        verify_error = 'Passwords do not match. Please try again.'
+        password = ''
+        verification = ''
+
+    if email == '':
+        email_error = ''
+    else:
+        if len(email) < 3 or len(email) > 20:
+            email_error = 'Email addresses must be between 3 and 20 characters.'
+
+    at = 0
+    dot = 0
+    for char in email:
+        if char == ' ':
+            email_error = 'Email addresses cannot include spaces.'
+        if char == '@':
+            at += 1
+        if char == '.':
+            dot += 1
+        if at > 1 or dot > 1:
+            email_error = "Invalid email format."
+        
+    if not user_error and not password_error and not verify_error and not email_error:
+        return redirect('/welcome?user={0}'.format(user))
+    else:
+        template = jinja_env.get_template('form.html')
+        return template.render(user_error=user_error, password_error=password_error, verify_error=verify_error, email_error=email_error, user=user, password=password, verification=verification, email=email)
+
+
+@app.route("/welcome")
+def welcome():
+    user = request.args.get('user')
+    template = jinja_env.get_template('welcome_message.html')
+    return template.render(user=user)
 
 app.run()
